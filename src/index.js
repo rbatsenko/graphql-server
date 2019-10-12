5937e74449878208222aff1e402f7976887fc302
Roman Batsenko <rbatsenko@gmail.com>
2019-10-12 16:30:10 +0200

    Install packages, add simple Schema, Resolvers and Apollo Server

--- src/index.js
+++ src/index.js
@@ -1,13 +1,30 @@
-import 'dotenv/config';
+import express from 'express';
+import { ApolloServer, gql } from 'apollo-server-express';
 
-const userCredentials = { firstname: 'Robin' };
-const userDetails = { nationality: 'German' };
+const app = express();
 
-const user = {
-  ...userCredentials,
-  ...userDetails,
+const schema = gql`
+  type Query {
+    me: User
+  }
+
+  type User {
+    username: String!
+  }
+`;
+const resolvers = {
+  Query: {
+    me: () => ({ username: 'Roman Batsenko' }),
+  },
 };
 
-console.log(user);
+const server = new ApolloServer({
+  typeDefs: schema,
+  resolvers,
+});
 
-console.log(process.env.SOME_ENV_VARIABLE);
+server.applyMiddleware({ app, path: '/graphql' });
+
+app.listen({ port: 8000 }, () => {
+  console.log('Apollo Server on http://localhost:8000/graphql');
+});
